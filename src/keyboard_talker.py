f6e3b8b00a0a7bed6ac7219ec0c72afa490ff445
linzhank <linzhank@gmail.com>
2018-05-03 17:06:36 -0400

    fix keyboard_talker

--- src/keyboard_talker.py
+++ src/keyboard_talker.py
@@ -10,28 +10,27 @@ def kb_talker():
   """Keyboard talker publishes keyboard input
   """
   pub = rospy.Publisher("keyboard_command", String, queue_size=10)
-  rospy.init_node("keypress", annoymous=True)
+  rospy.init_node("keypress", anonymous=True)
   rate = rospy.Rate(10)
+
   while not rospy.is_shutdown():
     cmd = getch.getch() # cmd will be a unicode
+
     # publish only 'a', 's', 'd', 'w'
     if cmd == 'a':
-      rospy.loginfo("{}: Turning left <".format(str(cmd)))
-      pub.publish(cmd)
+      rospy.loginfo("{}: Turning left <".format(cmd))
     elif cmd == 's':
-      rospy.loginfo("{}: Moving backward v".format(str(cmd)))
-      pub.publish(cmd)
+      rospy.loginfo("{}: Moving backward v".format(cmd))
     elif cmd == 'd':
-      rospy.loginfo("{}: Turning right >".format(str(cmd)))
-      pub.publish(cmd)
+      rospy.loginfo("{}: Turning right >".format(cmd))
     elif cmd == 'w':
-      rospy.loginfo("{}: Moving forward ^".format(str(cmd)))
-      pub.publish(cmd)
+      rospy.loginfo("{}: Moving forward ^".format(cmd))
     else:
       pass
+    pub.publish(cmd)
     rate.sleep()
 
-if __name__=="main":
+if __name__=='__main__':
   try:
     kb_talker()
   except rospy.ROSInterruptException:
