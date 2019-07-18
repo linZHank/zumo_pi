a6757be01882f6a41061dab0ca989204d1908df5
linzhank <linzhank@gmail.com>
2019-07-18 17:03:15 -0400

    create test script for keyboard control zumo

---
+++ scripts/zumo_keyboard_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from __future__ import absolute_import, print_function
+
+import rospy
+from zumo import Zumo
+
+zumo = Zumo()
+
+if __name__ == "__main__":
+    main()
+    zumo.run()
