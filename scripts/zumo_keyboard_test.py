4398c5e5b55e2f8ee47f440fc1d2fe9cc68f6ea5
linzhank <linzhank@gmail.com>
2019-07-19 15:10:35 -0400

    fix zumo driver class name

--- scripts/zumo_keyboard_test.py
+++ scripts/zumo_keyboard_test.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import, print_function
 import rospy
 from zumo import zumo_driver
 
-zumo = zumo_driver.Driver()
+zumo = zumo_driver.ZumoDriver()
 
 if __name__ == "__main__":
     zumo.run()
