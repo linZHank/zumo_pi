168e1a629ec6c634973af5ef4d08615203fda402
linzhank <linzhank@gmail.com>
2019-07-21 15:49:51 -0400

    change methods and properties in zumo_driver

--- scripts/zumo/zumo_driver.py
+++ scripts/zumo/zumo_driver.py
@@ -36,76 +36,76 @@ class ZumoDriver(object):
         rospy.init_node('zumo_driver', anonymous=True, log_level=log_level)
         rospy.loginfo("Initiate zumo_driver node.")
         # Initialize motor HAT and left, right motor.
-        self._mh = Adafruit_MotorHAT(addr)
-        self._left_motor = self._mh.getMotor(left_id)
-        self._right_motor = self._mh.getMotor(right_id)
-        self._left_trim = left_trim
-        self._right_trim = right_trim
-        self._left_speed_scale = 0
-        self._right_speed_scale = 0
-        self._left_motor.run(Adafruit_MotorHAT.RELEASE)
-        self._right_motor.run(Adafruit_MotorHAT.RELEASE) # start with motors turned off
+        self.mh = Adafruit_MotorHAT(addr)
+        self.left_motor = self.mh.getMotor(left_id)
+        self.right_motor = self.mh.getMotor(right_id)
+        self.left_trim = left_trim
+        self.right_trim = right_trim
+        self.left_speed_scale = 0
+        self.right_speed_scale = 0
+        self.left_motor.run(Adafruit_MotorHAT.RELEASE)
+        self.right_motor.run(Adafruit_MotorHAT.RELEASE) # start with motors turned off
         # ROS params
-        self._last_received = rospy.get_time()
-        self._timeout = rospy.get_param('~timeout', 5)
-        self._rate = rospy.get_param('~rate', 10)
-        self._max_speed = rospy.get_param('~max_speed', 0.5)
-        self._wheel_base = rospy.get_param('~wheel_base', 0.086)
+        self.last_received = rospy.get_time()
+        self.timeout = rospy.get_param('~timeout', 5)
+        self.rate = rospy.get_param('~rate', 10)
+        self.max_speed = rospy.get_param('~max_speed', 0.5)
+        self.wheel_base = rospy.get_param('~wheel_base', 0.086)
     	# Subscribe to /cmd_vel
     	rospy.Subscriber('cmd_vel', Twist, self._cmd_vel_callback)
         # Configure all motors to stop at program exit if desired.
         if stop_at_exit:
             atexit.register(self.stop)
 
-    def _cmd_vel_callback(self, data):
+    def cmd_vel_callback(self, data):
         """
         Callback function for Handling received data from '/cmd_vel' topic
         """
-        self._last_received = rospy.get_time()
+        self.last_received = rospy.get_time()
         # Extract linear and angular velocities from the message
         linear = data.linear.x
         angular = data.angular.z
         rospy.logdebug("cmd_vel: {}".format(data))
         # Calculate wheel speeds in m/s
-        left_speed = linear - angular*self._wheel_base/2
-        right_speed = linear + angular*self._wheel_base/2
+        left_speed = linear - angular*self.wheel_base/2
+        right_speed = linear + angular*self.wheel_base/2
         # Compute motor speed in scale of [0,255]
-        self._left_speed_scale = np.clip(int(255 * left_speed/self._max_speed), -255, 255)
-        self._right_speed_scale = np.clip(int(255 * right_speed/self._max_speed), -255, 255)
-        assert -255 <= self._left_speed_scale <= 255
-        assert -255 <= self._right_speed_scale <= 255
+        self.left_speed_scale = np.clip(int(255 * left_speed/self.max_speed), -255, 255)
+        self.right_speed_scale = np.clip(int(255 * right_speed/self.max_speed), -255, 255)
+        assert -255 <= self.left_speed_scale <= 255
+        assert -255 <= self.right_speed_scale <= 255
 
     def run(self):
         """
         Control loop of driving Zumo
         """
-        rate = rospy.Rate(self._rate)
+        rate = rospy.Rate(self.rate)
         while not rospy.is_shutdown():
             # Stop moving if no received new commands for a while
-            delay = rospy.get_time() - self._last_received
-            if delay < self._timeout:
-                if self._left_speed_scale >= 0: # set left motor speed and dir
-                    self._left_motor.setSpeed(self._left_speed_scale)
-                    self._left_motor.run(Adafruit_MotorHAT.FORWARD)
-                    rospy.logdebug("Left motor foward: {}".format(self._left_speed_scale))
+            delay = rospy.get_time() - self.last_received
+            if delay < self.timeout:
+                if self.left_speed_scale >= 0: # set left motor speed and dir
+                    self.left_motor.setSpeed(self.left_speed_scale)
+                    self.left_motor.run(Adafruit_MotorHAT.FORWARD)
+                    rospy.logdebug("Left motor foward: {}".format(self.left_speed_scale))
                 else:
-                    self._left_motor.setSpeed(np.abs(self._left_speed_scale))
-                    self._left_motor.run(Adafruit_MotorHAT.BACKWARD)
-                    rospy.logdebug("Left motor backward: {}".format(self._left_speed_scale))
-                if self._right_speed_scale >= 0: # set right motor speed and dir
-                    self._right_motor.setSpeed(self._right_speed_scale)
-                    self._right_motor.run(Adafruit_MotorHAT.FORWARD)
-                    rospy.logdebug("Right motor foward: {}".format(self._right_speed_scale))
+                    self.left_motor.setSpeed(np.abs(self.left_speed_scale))
+                    self.left_motor.run(Adafruit_MotorHAT.BACKWARD)
+                    rospy.logdebug("Left motor backward: {}".format(self.left_speed_scale))
+                if self.right_speed_scale >= 0: # set right motor speed and dir
+                    self.right_motor.setSpeed(self.right_speed_scale)
+                    self.right_motor.run(Adafruit_MotorHAT.FORWARD)
+                    rospy.logdebug("Right motor foward: {}".format(self.right_speed_scale))
                 else:
-                    self._right_motor.setSpeed(np.abs(self._right_speed_scale))
-                    self._right_motor.run(Adafruit_MotorHAT.BACKWARD)
-                    rospy.logdebug("Right motor backward: {}".format(self._right_speed_scale))
+                    self.right_motor.setSpeed(np.abs(self.right_speed_scale))
+                    self.right_motor.run(Adafruit_MotorHAT.BACKWARD)
+                    rospy.logdebug("Right motor backward: {}".format(self.right_speed_scale))
             else:
-                self._left_motor.run(Adafruit_MotorHAT.RELEASE)
-                self._right_motor.run(Adafruit_MotorHAT.RELEASE)
+                self.left_motor.run(Adafruit_MotorHAT.RELEASE)
+                self.right_motor.run(Adafruit_MotorHAT.RELEASE)
             rate.sleep()
 
     def stop(self):
         """Stop all movement."""
-        self._left_motor.run(Adafruit_MotorHAT.RELEASE)
-        self._right_motor.run(Adafruit_MotorHAT.RELEASE)
+        self.left_motor.run(Adafruit_MotorHAT.RELEASE)
+        self.right_motor.run(Adafruit_MotorHAT.RELEASE)
